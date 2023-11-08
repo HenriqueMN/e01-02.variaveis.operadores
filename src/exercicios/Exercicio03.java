a7ccbf4039bae866ac6958a41801a820bfc218d9
Henrique M. N. <henriquemancininevado@gmail.com>
2023-11-08 15:04:33 -0300

    Exercício 03

---
+++ src/exercicios/Exercicio03.java
@@ -0,0 +1,35 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio03 {
+
+	public static void main(String[] args) {
+		// Programa que lê o salário bruto, o adicional noturno, as horas extras e os descontos e retorna o salário líquido
+		
+		// Criação das variáveis e do Scanner
+		float salarioBruto, adicionalNoturno, horasExtras, descontos, salarioLiquido;
+		
+		Scanner input = new Scanner(System.in);
+		
+		// Leitura dos inputs
+		System.out.print("Insira o salário bruto: ");
+		salarioBruto = input.nextFloat();
+		
+		System.out.print("Insira o adicional noturno: ");
+		adicionalNoturno = input.nextFloat();
+		
+		System.out.print("Insira as horas extras: ");
+		horasExtras = input.nextFloat();
+		
+		System.out.print("Insira os descontos: ");
+		descontos = input.nextFloat();
+		
+		// Cálculo do salário líquido
+		salarioLiquido = salarioBruto + adicionalNoturno + (horasExtras * 5) - descontos;
+		
+		// Exibindo o salário líquido
+		System.out.println("Salário líquido: " + salarioLiquido);
+	}
+
+}
